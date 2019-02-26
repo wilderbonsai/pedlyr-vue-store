028235b01ef3eecda57a8fed454d66767e0a582b
Alan Nguyen <alan.t.ngn@gmail.com>
2019-02-26 19:44:05 +0100

    Add og title

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -68,7 +68,7 @@ export default {
         meta: [
           {
             'property': 'og:title',
-            'content':  this.productName,
+            'content': this.productName,
             'vmid': 'og:title'
           }
         ]
